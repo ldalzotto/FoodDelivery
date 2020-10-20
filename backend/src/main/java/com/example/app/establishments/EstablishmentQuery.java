7173f0626a5e4a308b5a759ccda7bc1f0eac6159
ldalzotto <loic.dalzotto@hotmail.fr>
2020-10-20 10:44:08 +0200

    REFACTOR - Establishment queries

--- backend/src/main/java/com/example/app/establishments/EstablishmentQuery.java
+++ backend/src/main/java/com/example/app/establishments/EstablishmentQuery.java
@@ -2,14 +2,14 @@ package com.example.app.establishments;
 
 import com.example.app.establishments.domain.*;
 import com.example.main.ConfigurationBeans;
+import com.example.utils.IntegerHeap;
 import org.springframework.dao.DataAccessException;
 import org.springframework.jdbc.support.GeneratedKeyHolder;
 import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.transaction.TransactionStatus;
 import org.springframework.transaction.support.DefaultTransactionDefinition;
 
-import java.sql.PreparedStatement;
-import java.sql.Types;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -19,12 +19,7 @@ public class EstablishmentQuery {
         EstablishmentAddress l_return = p_address.copy();
         KeyHolder l_addressKeyHolder = new GeneratedKeyHolder();
         ConfigurationBeans.jdbcTemplate.update(con -> {
-            PreparedStatement l_ps = con.prepareStatement("insert into establishment_address(street_full_name, city_id, lat, lng) VALUES (?,?,?,?)");
-            l_ps.setString(1, p_address.street_full_name);
-            l_ps.setLong(2, p_address.city_id);
-            l_ps.setDouble(3, p_address.lat);
-            l_ps.setDouble(4, p_address.lng);
-            return l_ps;
+            return EstablishmentQueryUtils.InsertEstablishmentAddress(con, p_address);
         }, l_addressKeyHolder);
         l_return.id = l_addressKeyHolder.getKey().longValue();
         return l_return;
@@ -44,22 +39,7 @@ public class EstablishmentQuery {
         Establishment l_return = p_establishment.copy();
         KeyHolder l_keyHolder = new GeneratedKeyHolder();
         ConfigurationBeans.jdbcTemplate.update(con -> {
-            PreparedStatement l_ps = con.prepareStatement("insert into establishments(name, address_id, phone, thumb_id, user_id) VALUES (?, ?, ?, ?, ?)");
-            l_ps.setString(1, p_establishment.name);
-            l_ps.setLong(2, p_establishment.address_id);
-            l_ps.setString(3, p_establishment.phone);
-
-            if(p_establishment.thumb_id!=null)
-            {
-                l_ps.setLong(4, p_establishment.thumb_id);
-            }
-            else
-            {
-                l_ps.setNull(4, Types.INTEGER);
-            }
-
-            l_ps.setLong(5, p_establishment.user_id);
-            return l_ps;
+            return EstablishmentQueryUtils.InsertEstablishment(con, p_establishment);
         }, l_keyHolder);
         l_return.id = l_keyHolder.getKey().longValue();
         return l_return;
@@ -73,14 +53,7 @@ public class EstablishmentQuery {
             l_ps.setLong(1, p_establihsmentId);
             return l_ps;
         }, (rs, rowNum) -> {
-            Establishment l_establishment = new Establishment();
-            l_establishment.id = rs.getLong(1);
-            l_establishment.name = rs.getString(2);
-            l_establishment.address_id = rs.getLong(3);
-            l_establishment.phone = rs.getString(4);
-            l_establishment.thumb_id = rs.getLong(5);
-            l_establishment.user_id = rs.getLong(6);
-            return l_establishment;
+            return EstablishmentQueryUtils.RetrieveEstablishment(rs, new IntegerHeap(1));
         });
 
         if(l_retrievedEstablishment.size() > 0)
@@ -108,22 +81,11 @@ public class EstablishmentQuery {
                 l_ps.setDouble(4, p_maxlng);
                 return l_ps;
             }, (rs) -> {
+                IntegerHeap l_rs_index = new IntegerHeap(1);
                 EstablishmentWithDependenciesV2 l_est = new EstablishmentWithDependenciesV2();
 
-                Establishment l_establishment = new Establishment();
-                l_establishment.id = rs.getLong(1);
-                l_establishment.name = rs.getString(2);
-                l_establishment.address_id = rs.getLong(3);
-                l_establishment.phone = rs.getString(4);
-                l_establishment.thumb_id = rs.getLong(5);
-                l_establishment.user_id = rs.getLong(6);
-
-                EstablishmentAddress l_establishmentAddress = new EstablishmentAddress();
-                l_establishmentAddress.id = rs.getLong(7);
-                l_establishmentAddress.street_full_name = rs.getString(8);
-                l_establishmentAddress.city_id = rs.getLong(9);
-                l_establishmentAddress.lat = rs.getFloat(10);
-                l_establishmentAddress.lng = rs.getFloat(11);
+                Establishment l_establishment = EstablishmentQueryUtils.RetrieveEstablishment(rs, l_rs_index);
+                EstablishmentAddress l_establishmentAddress = EstablishmentQueryUtils.RetrieveEstablishmentAddress(rs, l_rs_index);
 
                 l_foundEstablishments.establishments.add(l_establishment);
                 l_foundEstablishments.establishment_addresses.add(l_establishmentAddress);
@@ -139,22 +101,11 @@ public class EstablishmentQuery {
                     l_ps.setLong(1, p_establishment_id);
                     return l_ps;
                 }, (rs, rowNum) -> {
+                    IntegerHeap l_rs_index = new IntegerHeap(1);
+
                     EstablishmentWithAddress l_establishmentWithAddress = new EstablishmentWithAddress();
-
-                    l_establishmentWithAddress.establishment = new Establishment();
-                    l_establishmentWithAddress.establishment.id = rs.getLong(1);
-                    l_establishmentWithAddress.establishment.name = rs.getString(2);
-                    l_establishmentWithAddress.establishment.address_id = rs.getLong(3);
-                    l_establishmentWithAddress.establishment.phone = rs.getString(4);
-                    l_establishmentWithAddress.establishment.thumb_id = rs.getLong(5);
-                    l_establishmentWithAddress.establishment.user_id = rs.getLong(6);
-
-                    l_establishmentWithAddress.establishment_address = new EstablishmentAddress();
-                    l_establishmentWithAddress.establishment_address.id = rs.getLong(7);
-                    l_establishmentWithAddress.establishment_address.street_full_name = rs.getString(8);
-                    l_establishmentWithAddress.establishment_address.city_id = rs.getLong(9);
-                    l_establishmentWithAddress.establishment_address.lat = rs.getFloat(10);
-                    l_establishmentWithAddress.establishment_address.lng = rs.getFloat(11);
+                    l_establishmentWithAddress.establishment = EstablishmentQueryUtils.RetrieveEstablishment(rs, l_rs_index);
+                    l_establishmentWithAddress.establishment_address = EstablishmentQueryUtils.RetrieveEstablishmentAddress(rs, l_rs_index);
 
                     return l_establishmentWithAddress;
                 });
@@ -177,22 +128,7 @@ public class EstablishmentQuery {
                 return l_ps;
             });
             ConfigurationBeans.jdbcTemplate.update(con -> {
-                PreparedStatement l_ps = con.prepareStatement("insert into establishments(id, name, address_id, phone, thumb_id, user_id) values (?,?,?,?,?,?)");
-                l_ps.setLong(1, p_establishment.id);
-                l_ps.setString(2, p_establishment.name);
-                l_ps.setLong(3, p_establishment.address_id);
-                l_ps.setString(4, p_establishment.phone);
-                if(p_establishment.thumb_id != null)
-                {
-                    l_ps.setLong(5, p_establishment.thumb_id);
-                }
-                else
-                {
-                    l_ps.setNull(5, Types.INTEGER);
-                }
-
-                l_ps.setLong(6, p_establishment.user_id);
-                return l_ps;
+                return EstablishmentQueryUtils.InsertEstablishment_withId(con, p_establishment);
             });
 
             ConfigurationBeans.transactionManager.commit(ts);
@@ -217,13 +153,7 @@ public class EstablishmentQuery {
             });
 
             ConfigurationBeans.jdbcTemplate.update(con -> {
-                PreparedStatement l_ps = con.prepareStatement("insert into establishment_address(id, street_full_name, city_id, lat, lng) VALUES (?,?,?,?,?)");
-                l_ps.setLong(1, p_establishmentAddress.id);
-                l_ps.setString(2, p_establishmentAddress.street_full_name);
-                l_ps.setLong(3, p_establishmentAddress.city_id);
-                l_ps.setDouble(4, p_establishmentAddress.lat);
-                l_ps.setDouble(5, p_establishmentAddress.lng);
-                return l_ps;
+                return EstablishmentQueryUtils.InsertEstablishmentAddress_withId(con, p_establishmentAddress);
             });
 
             ConfigurationBeans.transactionManager.commit(ts);
@@ -246,22 +176,9 @@ public class EstablishmentQuery {
                 l_ps.setLong(1, p_user_id);
                 return l_ps;
             }, (rs) -> {
-                Establishment l_establishment = new Establishment();
-
-                l_establishment.id = rs.getLong(1);
-                l_establishment.name = rs.getString(2);
-                l_establishment.address_id = rs.getLong(3);
-                l_establishment.phone = rs.getString(4);
-                l_establishment.thumb_id = rs.getLong(5);
-                l_establishment.user_id = rs.getLong(6);
-
-                EstablishmentAddress l_establishmentAddress = new EstablishmentAddress();
-
-                l_establishmentAddress.id = rs.getLong(7);
-                l_establishmentAddress.street_full_name = rs.getString(8);
-                l_establishmentAddress.city_id = rs.getLong(9);
-                l_establishmentAddress.lat = rs.getFloat(10);
-                l_establishmentAddress.lng = rs.getFloat(11);
+                IntegerHeap l_rs_index = new IntegerHeap(1);
+                Establishment l_establishment = EstablishmentQueryUtils.RetrieveEstablishment(rs, l_rs_index);
+                EstablishmentAddress l_establishmentAddress = EstablishmentQueryUtils.RetrieveEstablishmentAddress(rs, l_rs_index);
 
                 l_return.establishments.add(l_establishment);
                 l_return.establishment_addresses.add(l_establishmentAddress);
@@ -287,6 +204,93 @@ public class EstablishmentQuery {
             });
         }
     }
+}
 
 
+class EstablishmentQueryUtils
+{
+    public static PreparedStatement InsertEstablishment_withId(Connection con, Establishment p_establishment) throws SQLException
+    {
+        PreparedStatement l_ps = con.prepareStatement("insert into establishments(id, name, address_id, phone, thumb_id, user_id) values (?,?,?,?,?,?)");
+        l_ps.setLong(1, p_establishment.id);
+        l_ps.setString(2, p_establishment.name);
+        l_ps.setLong(3, p_establishment.address_id);
+        l_ps.setString(4, p_establishment.phone);
+        if(p_establishment.thumb_id != null)
+        {
+            l_ps.setLong(5, p_establishment.thumb_id);
+        }
+        else
+        {
+            l_ps.setNull(5, Types.INTEGER);
+        }
+
+        l_ps.setLong(6, p_establishment.user_id);
+        return l_ps;
+    }
+
+    public static PreparedStatement InsertEstablishment(Connection con, Establishment p_establishment) throws SQLException
+    {
+        PreparedStatement l_ps = con.prepareStatement("insert into establishments(name, address_id, phone, thumb_id, user_id) values (?,?,?,?,?)");
+        l_ps.setString(1, p_establishment.name);
+        l_ps.setLong(2, p_establishment.address_id);
+        l_ps.setString(3, p_establishment.phone);
+        if(p_establishment.thumb_id != null)
+        {
+            l_ps.setLong(4, p_establishment.thumb_id);
+        }
+        else
+        {
+            l_ps.setNull(4, Types.INTEGER);
+        }
+
+        l_ps.setLong(5, p_establishment.user_id);
+        return l_ps;
+    }
+
+    public static Establishment RetrieveEstablishment(ResultSet p_rs, IntegerHeap p_startIndex) throws SQLException
+    {
+        Establishment l_establishment = new Establishment();
+
+        l_establishment.id = p_rs.getLong(p_startIndex.number++);
+        l_establishment.name = p_rs.getString(p_startIndex.number++);
+        l_establishment.address_id = p_rs.getLong(p_startIndex.number++);
+        l_establishment.phone = p_rs.getString(p_startIndex.number++);
+        l_establishment.thumb_id = p_rs.getLong(p_startIndex.number++);
+        l_establishment.user_id = p_rs.getLong(p_startIndex.number++);
+
+        return l_establishment;
+    }
+
+    public static PreparedStatement InsertEstablishmentAddress_withId(Connection con, EstablishmentAddress p_establishmentAddress) throws SQLException
+    {
+        PreparedStatement l_ps = con.prepareStatement("insert into establishment_address(id, street_full_name, city_id, lat, lng) VALUES (?,?,?,?,?)");
+        l_ps.setLong(1, p_establishmentAddress.id);
+        l_ps.setString(2, p_establishmentAddress.street_full_name);
+        l_ps.setLong(3, p_establishmentAddress.city_id);
+        l_ps.setDouble(4, p_establishmentAddress.lat);
+        l_ps.setDouble(5, p_establishmentAddress.lng);
+        return l_ps;
+    }
+
+    public static PreparedStatement InsertEstablishmentAddress(Connection con, EstablishmentAddress p_establishmentAddress) throws SQLException
+    {
+        PreparedStatement l_ps = con.prepareStatement("insert into establishment_address(street_full_name, city_id, lat, lng) VALUES (?,?,?,?)");
+        l_ps.setString(1, p_establishmentAddress.street_full_name);
+        l_ps.setLong(2, p_establishmentAddress.city_id);
+        l_ps.setDouble(3, p_establishmentAddress.lat);
+        l_ps.setDouble(4, p_establishmentAddress.lng);
+        return l_ps;
+    }
+
+    public static EstablishmentAddress RetrieveEstablishmentAddress(ResultSet p_rs, IntegerHeap p_startIndex) throws SQLException
+    {
+        EstablishmentAddress l_establishmentAddress = new EstablishmentAddress();
+        l_establishmentAddress.id = p_rs.getLong(p_startIndex.number++);
+        l_establishmentAddress.street_full_name = p_rs.getString(p_startIndex.number++);
+        l_establishmentAddress.city_id = p_rs.getLong(p_startIndex.number++);
+        l_establishmentAddress.lat = p_rs.getFloat(p_startIndex.number++);
+        l_establishmentAddress.lng = p_rs.getFloat(p_startIndex.number++);
+        return l_establishmentAddress;
+    }
 }
---
+++ backend/src/main/java/com/example/utils/IntegerHeap.java
@@ -0,0 +1,10 @@
+package com.example.utils;
+
+public class IntegerHeap
+{
+    public int number;
+
+    public IntegerHeap(int number) {
+        this.number = number;
+    }
+}
